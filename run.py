36063442e1c85f180210c32df278607350710bbd
cewareham <cwareham007@gmail.com>
2022-02-14 22:23:57 -0700

    #2 Level 1 - Nodes

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.locals import *
 from constants import *
 from pacman import Pacman
+from nodes import NodeGroup
 
 class GameController(object):
     def __init__(self):
@@ -16,6 +17,8 @@ class GameController(object):
 
     def startGame(self):
         self.setBackground()
+        self.nodes = NodeGroup()
+        self.nodes.setupTestNodes()
         self.pacman = Pacman()
 
     def update(self):
@@ -32,6 +35,7 @@ class GameController(object):
 
     def render(self):
         self.screen.blit(self.background, (0, 0))
+        self.nodes.render(self.screen)
         self.pacman.render(self.screen)
         pygame.display.update()
 
